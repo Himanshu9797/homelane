9257edfe5740875fd9e23ae6f370505767713979
hdassosiation@gmail.com <hdassosiation@gmail.com>
2021-08-09 06:24:22 +0530

    3 api's

--- Covid3Wave/coviddata/urls.py
+++ Covid3Wave/coviddata/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import DateInfo
+from .views import DateInfo,StateInfo,PinpointState,PinpointInfo
 
 urlpatterns = [
     path('Get_date_info(<Date>)/', DateInfo.as_view()),
+    path('Get_state_info(<state>)/', StateInfo.as_view()),
+    path('Pinpoint_state(<state>,<Date>)/', PinpointState.as_view()),
+    path('Pinpoint_info(<Date>,<state>)/', PinpointInfo.as_view())
 ]
--- Covid3Wave/coviddata/views.py
+++ Covid3Wave/coviddata/views.py
@@ -19,3 +19,44 @@ class DateInfo(View):
         }
         return JsonResponse(data, status=200)
 
+
+class StateInfo(View):
+    def get(self, request, state):
+
+        vaccineStatewise = CovidVaccineStatewise.objects.filter(state=state).order_by('updated_on')
+        testingStatewise = StatewiseTestingDetails.objects.filter(state=state).order_by('date')
+        cases = Covid19cases.objects.filter(state=state).order_by('date')
+
+        data = {
+            "vaccineStatewise": serializers.serialize('json', vaccineStatewise),
+            "testingStatewise": serializers.serialize('json', testingStatewise),
+            "cases": serializers.serialize('json', cases),
+        }
+        return JsonResponse(data, status=200)
+
+class PinpointState(View):
+    def get(self, request, *args, **kwargs):
+        vaccineStatewise = CovidVaccineStatewise.objects.filter(state=kwargs['state'], updated_on=kwargs['Date']).order_by('updated_on')
+        testingStatewise = StatewiseTestingDetails.objects.filter(state=kwargs['state'], date=kwargs['Date']).order_by('date')
+        cases = Covid19cases.objects.filter(state=kwargs['state'], date=kwargs['Date']).order_by('date')
+
+        data = {
+            "vaccineStatewise": serializers.serialize('json', vaccineStatewise),
+            "testingStatewise": serializers.serialize('json', testingStatewise),
+            "cases": serializers.serialize('json', cases),
+        }
+        return JsonResponse(data, status=200)
+
+class PinpointInfo(View):
+    def get(self, request, *args, **kwargs):
+
+        vaccineStatewise = CovidVaccineStatewise.objects.filter(state__in=kwargs['state'].split(","), updated_on=kwargs['Date']).order_by('updated_on')
+        testingStatewise = StatewiseTestingDetails.objects.filter(state__in=kwargs['state'].split(","), date=kwargs['Date']).order_by('date')
+        cases = Covid19cases.objects.filter(state__in=kwargs['state'].split(","), date=kwargs['Date']).order_by('date')
+
+        data = {
+            "vaccineStatewise": serializers.serialize('json', vaccineStatewise),
+            "testingStatewise": serializers.serialize('json', testingStatewise),
+            "cases": serializers.serialize('json', cases),
+        }
+        return JsonResponse(data, status=200)
